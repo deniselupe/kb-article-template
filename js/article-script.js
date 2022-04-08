438bffed97c7d227173c19c0d0b0e132cf4614ae
Denise Rodriguez <64881284+deniselupe@users.noreply.github.com>
2022-04-08 14:40:12 -0500

    fix: hide nav list if window.innerWidth >= 1150

--- js/article-script.js
+++ js/article-script.js
@@ -1,7 +1,37 @@
-//Search Bar Scroll Behavior
+// Search Bar Scroll Behavior
 const searchBar = document.querySelector("form");
 const offset = searchBar.getBoundingClientRect();
+const menuButton = document.querySelector('.menu-button');
+const smallMenu = document.querySelector('.small-nav');
 
+// Scroll Up Button Functionality
+const scrollUp = document.querySelector('.scroll-up');
+scrollUp.addEventListener('click', function () {
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth'
+	});
+});
+
+// Hides collapsed navigation menu when window width increases to 1150px or bigger
+window.addEventListener('resize', function () {
+  if (window.innerWidth >= 1150) {
+    if (smallMenu.getAttribute('style')) {
+        smallMenu.removeAttribute('style');
+    }
+  }
+});
+
+// Toggle to make the collapsed navigation menu hide/appear when user selects menu button
+menuButton.addEventListener('click', function () {
+	if (smallMenu.getAttribute('style')) {
+		smallMenu.removeAttribute('style');
+	} else if (!smallMenu.getAttribute('style')) {
+		smallMenu.style.display = 'block';
+	}
+});
+
+// Functionality for Search Bar
 function stickyPosition () {
 	if (window.pageYOffset > offset.top) {
 		searchBar.style.position = 'fixed';
@@ -14,18 +44,7 @@ function stickyPosition () {
 
 window.addEventListener('scroll', stickyPosition);
 
-const menuButton = document.querySelector('.menu-button');
-const smallMenu = document.querySelector('.small-nav');
-
-menuButton.addEventListener('click', function () {
-	if (smallMenu.getAttribute('style')) {
-		smallMenu.removeAttribute('style');
-	} else if (!smallMenu.getAttribute('style')) {
-		smallMenu.style.display = 'block';
-	}
-});
-
-//Expandable Div Script
+// Expandable Div Script
 let expandContainer = document.querySelectorAll('.expand-container');
 expandContainer = Array.from(expandContainer);
 
@@ -38,7 +57,7 @@ expandContainer.forEach((container) => {
 	let expandButton = container.firstElementChild;
 	let expandContent = container.lastElementChild;
 	let expandButtonImg = expandButton.firstElementChild;
-	
+
 	expandButton.addEventListener('click', function () {
 		if (!expandContent.getAttribute('style') === true) {
 			expandButtonImg.setAttribute('src', './images/add-icon-white.png');
@@ -49,14 +68,5 @@ expandContainer.forEach((container) => {
 			expandButton.setAttribute('style', 'border:2px solid #5a189a');
 			expandContent.removeAttribute('style');
 		}
-	});
-});
-
-//Scroll Up Button Functionality
-const scrollUp = document.querySelector('.scroll-up');
-scrollUp.addEventListener('click', function () {
-	window.scrollTo({
-		top: 0,
-		behavior: 'smooth'
 	});
 });
